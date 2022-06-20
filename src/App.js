526e12f2ca6d0a943f9b3b8c5238298ec8d86b5a
Nathan Schmidt <natepsacc@gmail.com>
2022-06-20 11:36:34 -0700

    theology

--- src/App.js
+++ src/App.js
@@ -100,9 +100,20 @@ class App extends Component{
 <div className="footer">
 <h2>NOTICE</h2>
 <h1>
-*used free hosting service for svg that required me to update after a certain amount of traffic. not doing that anymore :-)  eventually i will put a hq png or  make a smaller svg file*
+*used free hosting service for svg that required me to update after a certain amount of traffic. not doing that anymore :-)  HQ but not scannable .JPG copy below*
 </h1>
 </div>
+<LazyLoadImage
+className='lqjpg'
+style={imgStyle}
+ref={this.imgRef}
+	 threshhold='1000'
+	 placeholderSrc="https://storageapi.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/gif/loading.gif"
+
+			effect="blur"
+		 alt="A Sheet of red QR codes that contain the Four Gospels.  The Red QR codes are clustered in 4 seperate grids, with each grid resembling a depction of the Author when viewed in entirety."
+		 src="https://i.imgur.com/XA77NIn.jpg" // use normal <img> attributes as props
+			/>
       </div>
     )
   }
